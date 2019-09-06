45d7dceddf80ea02486a1593b46a528de0096465
Chai Ling Lee <lingleechai@Chais-MacBook-Pro.local>
2019-09-06 18:10:04 +0800

    API call added

--- instagram_api/blueprints/users/views.py
+++ instagram_api/blueprints/users/views.py
@@ -1,4 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from models.image import Image
+from models.user import User
+
 
 users_api_blueprint = Blueprint('users_api',
                              __name__,
@@ -6,4 +9,33 @@ users_api_blueprint = Blueprint('users_api',
 
 @users_api_blueprint.route('/', methods=['GET'])
 def index():
-    return "USERS API"
+    # order follow by alphabet
+    users = User.select()
+    response = {
+    "id" : [user.id for user in users],
+    "email" : [user.email for user in users],
+    "username" : [user.username for user in users]
+    }
+    return jsonify(response)
+    # return "USERS API"
+
+@users_api_blueprint.route('/images/', methods=['GET'])
+def image():
+    images = Image.select().where(Image.user_id == request.args.get('userId'))
+    user = User.get_by_id(request.args.get('userId'))
+    response = {
+        "user" : user.username,
+        "images":["https://linglee-nextagram.s3-ap-southeast-1.amazonaws.com/userImg/" + image.userImg for image in images]
+    }
+    return jsonify(response)
+
+
+
+# @users_api_blueprint.route('/login/', methods=['POST'])
+# def login():
+#     users = User.select()
+#     response ={
+#     "username" : [user.username for user in users],
+#     "password" : [user.password for user in users]
+#     }
+#     return jsonify(response)
